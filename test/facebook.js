eb9c799b03282e8a4abd644fa436310917f1a751
Ankur Rastogi <ankur.rastogi@paytm.com>
2016-07-22 10:42:57 +0530

    modified test file

--- test/facebook.js
+++ test/facebook.js
@@ -7,9 +7,9 @@ describe("#Social Service Check", function(){
 	it("#Facebook Service Auto Post", function(done){
 		this.timeout(0);
 		var options = {
-			//link : "http://www.oyewiki.com/tutorials/how-to-pay-electricity-bill-online"
-			"message" : "Oyewiki Engineering: Working on node module to auto post on all facebook, twitter, quora, google plus etc social account. Started from facebook. Code coming soon on github.",
-			"pages" : ["oyewiki"]
+			link : "http://www.businessinsider.in/This-floating-underwater-tunnel-in-Norway-could-save-half-a-day-in-travel-time/articleshow/53325896.cms"
+			//"message" : "Oyewiki Engineering: Working on node module to auto post on all facebook, twitter, quora, google plus etc social account. Started from facebook. Code coming soon on github.",
+			//"pages" : ["oyewiki"]
 		};
 		socialServices.facebook.publishPost(options, function(err, result){
 			console.log(err, result);
